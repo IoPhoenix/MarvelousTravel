9c7fee5bb3f02f61bb6330dbfc72d6cf30e673d6
IoPhoenix <olga.fomin26@gmail.com>
2017-10-30 18:35:54 -0700

    Organized gulpfile.js

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,5 +9,9 @@ cssImport = require('postcss-import');
 gulp.task('styles', function() {
 return gulp.src('./app/assets/styles/styles.css')
             .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-            .pipe(gulp.dest('./app/temp/styles'))
+            .on('error', function(errorInfo) {
+                console.log(errorInfo.toString());
+                this.emit('end');
+            })
+            .pipe(gulp.dest('./app/temp/styles'));
 });

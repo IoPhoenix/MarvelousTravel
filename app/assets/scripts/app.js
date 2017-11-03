bca4d3477720fbc1e8f3943c98b2c6ff0a132dd5
IoPhoenix <olga.fomin26@gmail.com>
2017-11-03 13:06:44 -0700

    Completed reveal on scroll behavior

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 const mobileMenu = new MobileMenu();
+const revealOnScroll = new RevealOnScroll();

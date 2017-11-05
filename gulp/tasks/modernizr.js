dc4848ae5e51aab31229197082c24283b42f9b77
IoPhoenix <olga.fomin26@gmail.com>
2017-11-05 00:55:31 -0700

    Completed gulp build task

---
+++ gulp/tasks/modernizr.js
@@ -0,0 +1,14 @@
+// Modernizr can test browsers for support of different feature 
+
+const gulp = require('gulp'),
+        modernizr = require('gulp-modernizr');
+
+gulp.task('modernizr', function() {
+    return gulp.src(['./app/assets/styles/**/*.css', './app/assets/scripts/**/*.js'])
+            .pipe(modernizr({
+                "options": [
+                    "setClasses"
+                ]
+            }))
+            .pipe(gulp.dest('./app/temp/scripts'));
+});
